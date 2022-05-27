071074ee5acebb454f1ad9f1022404439846d035
Drob08 <Darwin.robinson8@gmail.com>
2022-05-26 20:29:25 -0400

    fixing postgres error casting column type integer

--- db/migrate/20220517174227_change_booking_status_to_be_integer_in_reservations.rb
+++ db/migrate/20220517174227_change_booking_status_to_be_integer_in_reservations.rb
@@ -1,5 +1,5 @@
 class ChangeBookingStatusToBeIntegerInReservations < ActiveRecord::Migration[6.1]
   def change
-    change_column :reservations, :booking_status, :integer
+    change_column :reservations, :booking_status, 'integer USING CAST(booking_status AS integer)'
   end
 end
